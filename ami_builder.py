90205121c0b14d6ee8f113504c0d1b119fff4bdc
Matt Burgess <matthewburgess@digital.uc.dwp.gov.uk>
2019-01-14 10:54:30 +0000

    Add logging config

--- ami_builder.py
+++ ami_builder.py
@@ -1,13 +1,24 @@
 import botocore
 import boto3
 import jinja2
+import logging
 import os
 import subprocess
 import sys
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def handler(event, context):
+    if event['log_level']:
+        try:
+            logger.setLevel(logging.getLevelName(event['log_level'].upper()))
+        except ValueError:
+            logger.warning(f"Invalid log level specified: {event['log_level']}; using INFO")
+
     if event['packer_template_bucket_region'] and event['packer_template_bucket'] and event['packer_template_key']:
         s3_url=f"https://s3.{event['packer_template_bucket_region']}.amazonaws.com"
+        logger.info(f"Getting packer template from {s3_url}/{event['packer_template_bucket']}/{event['packer_template_key']}")
         s3 = boto3.resource('s3', event['packer_template_bucket_region'],
                             endpoint_url=s3_url,
                             config=botocore.config.Config(s3={'addressing_style':'path'}))
