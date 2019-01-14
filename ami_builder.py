eb2a8b3c45709a8783e9aa3270aff50d266f76e8
Matt Burgess <matthewburgess@digital.uc.dwp.gov.uk>
2019-01-14 12:13:54 +0000

    Various fixes

--- ami_builder.py
+++ ami_builder.py
@@ -16,15 +16,17 @@ def handler(event, context):
         except ValueError:
             logger.warning(f"Invalid log level specified: {event['log_level']}; using INFO")
 
-    if logger.getEffectiveLevel() == logging.DEBUG:
+    if logger.isEnabledFor(logging.DEBUG):
         # Log everything from boto3
         boto3.set_stream_logger()
         logger.debug(f"Using boto3 {boto3.__version__}")
 
-    if event['packer_template_bucket_region'] and event['packer_template_bucket'] and event['packer_template_key']:
-        s3_url=f"https://s3.{event['packer_template_bucket_region']}.amazonaws.com"
+    if 'packer_template_bucket_region' in event:
+        s3_url = f"https://s3.{event['packer_template_bucket_region']}.amazonaws.com"
+
+    if event['packer_template_bucket'] and event['packer_template_key']:
         logger.info(f"Getting packer template from {s3_url}/{event['packer_template_bucket']}/{event['packer_template_key']}")
-        s3 = boto3.resource('s3', event['packer_template_bucket_region'],
+        s3 = boto3.resource('s3',
                             endpoint_url=s3_url,
                             config=botocore.config.Config(s3={'addressing_style':'path'}))
         s3.Bucket(event['packer_template_bucket']).download_file(event['packer_template_key'], '/tmp/packer_template.json.j2')
@@ -37,12 +39,15 @@ def handler(event, context):
     with open('/tmp/packer.json', 'w') as packer_file:
         packer_file.write(template.render(event=event))
 
-    if event['provision_script_bucket_region'] and event['provision_script_bucket'] and event['provision_script_keys']:
-        s3_url=f"https://s3.{event['provision_script_bucket_region']}.amazonaws.com"
-        s3 = boto3.resource('s3', event['provision_script_bucket_region'],
+    if 'provision_script_bucket_region' in event:
+        s3_url = f"https://s3.{event['provision_script_bucket_region']}.amazonaws.com"
+
+    if event['provision_script_bucket'] and event['provision_script_keys']:
+        s3 = boto3.resource('s3',
                             endpoint_url=s3_url,
                             config=botocore.config.Config(s3={'addressing_style':'path'}))
         for script in event['provision_script_keys']:
+            logger.info(f"Getting provision script from {s3_url}/{event['provision_script_bucket']}/{script}")
             s3.Bucket(event['provision_script_bucket']).download_file(script, f'/tmp/{script}')
 
     try:
