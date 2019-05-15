ef625ae84af9231f7e9a9b631cb68b62acde6c41
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-05-15 09:48:58 +0100

    add logging to view results of packer.json

--- ami_builder.py
+++ ami_builder.py
@@ -5,10 +5,19 @@ import logging
 import os
 import subprocess
 import json
+import sys
 from botocore.exceptions import ClientError
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# Initialise logging
+logger = logging.getLogger(__name__)
+log_level = os.environ["LOG_LEVEL"] if "LOG_LEVEL" in os.environ else "ERROR"
+logger.setLevel(logging.getLevelName(log_level.upper()))
+logging.basicConfig(
+    stream=sys.stdout,
+    format="%(asctime)s %(levelname)s %(module)s "
+           "%(process)s[%(thread)s] %(message)s",
+)
+logger.info("Logging at {} level".format(log_level.upper()))
 
 
 def handler(event, context):
@@ -24,8 +33,9 @@ def handler(event, context):
     if logger.isEnabledFor(logging.DEBUG):
         # Log everything from boto3
         boto3.set_stream_logger()
-        logger.debug(f"Using boto3 {boto3.__version__}")
+    logger.debug(f"Using boto3 {boto3.__version__}")
 
+    logger.debug(event)
     s3_url = f"https://s3.{event['packer_template_bucket_region']}.amazonaws.com"
 
     if event['packer_template_bucket'] and event['packer_template_key']:
@@ -46,6 +56,8 @@ def handler(event, context):
         template = jinja2.Template(in_template.read())
     with open(f'{download_dir}/packer.json', 'w') as packer_file:
         packer_file.write(template.render(event=event, download_dir=download_dir))
+        logger.info(template)
+
 
     if 'provision_script_bucket_region' in event:
         s3_url = f"https://s3.{event['provision_script_bucket_region']}.amazonaws.com"
