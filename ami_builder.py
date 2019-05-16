49e7971cb790b9b6d7fc5acfd1be69f110785900
ian douglas <iandouglas@digital.uc.dwp.gov.uk>
2019-05-16 10:49:49 +0100

    changed w to + for r/w when opening log file

--- ami_builder.py
+++ ami_builder.py
@@ -54,7 +54,7 @@ def handler(event, context):
 
     with open(f'{download_dir}/packer_template.json.j2') as in_template:
         template = jinja2.Template(in_template.read())
-    with open(f'{download_dir}/packer.json', 'w') as packer_file:
+    with open(f'{download_dir}/packer.json', '+') as packer_file:
         packer_file.write(template.render(event=event, download_dir=download_dir))
         logger.debug(packer_file.read())
 
@@ -105,12 +105,10 @@ if __name__ == "__main__":
     json_content = json.loads(open('event.json', 'r').read())
     try:
         handler(json_content, None)
-    except KeyError as key_name:
-        logger.error(f'Key: {key_name} is required in payload')
-if __name__ == "__main__":
-    try:
-        json_content = json.loads(open("event.json", "r").read())
     except Exception as e:
         logger.error(e)
         sys.exit(1)
-    handler(json_content, None)
+    except KeyError as key_name:
+        logger.error(f'Key: {key_name} is required in payload')
+        sys.exit(1)
+
